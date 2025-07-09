444e997b0e548feff467daa018e7f344ece1d0fc
alxthedolphin <alexjamesdolphin@gmail.com>
2025-07-09 16:56:43 +0100

    Setup stega connection ready for Vercel
    Connect to sanity via token
    
    Enable sanity live editing (customer benefits from easy and rapid editing of the store)
    
    Reorg of env vars

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,12 @@ export const client = createClient({
   dataset,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  // https://www.sanity.io/docs/stega
+  stega: {
+    // if deployed on vercel, use the vercel url
+    // if not, use the base url
+    studioUrl: process.env.VERCEL_URL
+     ? `https://${process.env.VERCEL_URL}/studio` 
+     : `${process.env.NEXT_PUBLIC_BASE_URL}/studio`,
+  }
 })
