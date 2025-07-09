7511c5d2b59146723157d1fd830617e57e7ecb52
alxthedolphin <alexjamesdolphin@gmail.com>
2025-07-09 17:31:44 +0100

    Create get all products and categories GROQ queries
    Run typegen to typesafe it
    
    Scaffold product view with category selector and product grid to come

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,10 +1,23 @@
-import { Button } from "@/components/ui/button";
+import ProductsView from "@/components/ProductsView";
+import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 
-export default function Home() {
+export default async function Home() {
+
+  const products = await getAllProducts();
+  const categories = await getAllCategories();
+
+  //console.log(
+    // crypto.randomUUID().slice(0,5)+
+    // `> rerender with ${products.length} products`
+  //);
+  
   return (
     <div >
-     <h1>Hello World 11</h1>
-     <Button>Word</Button>
+      <h1>Hello World 11</h1>
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+        <ProductsView products={products} categories={[]} />
+      </div>
     </div>
   );
 }
