4f9a5b398ca89514ac2b80ff360338de64e0d597
alx-dolphin <alexjamesdolphin@gmail.com>
2025-10-03 21:55:38 +0100

    Restore Sanity Live
    Update sanity to latest
    Use new components

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -3,7 +3,7 @@
 // https://github.com/sanity-io/next-sanity#live-content-api 
 
 import "server-only";
-import { defineLive } from "next-sanity";
+import { defineLive } from "next-sanity/live";
 import { client } from './client'
 
 const token = process.env.SANITY_API_READ_TOKEN;
